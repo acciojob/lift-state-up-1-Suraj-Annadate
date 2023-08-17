561827882824792f3c477bd9c2f4b46d76f2397a
suraj0698 <annadatesuraj@gmail.com>
2023-08-17 16:32:03 +0530

    done2

--- src/components/Child.js
+++ src/components/Child.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Child = ({ showModal, onButtonClick }) => {
   return (
     <div className="child">
-      <h2>Child Component</h2>
+      <h3>Child Component</h3>
       <button  className='btn' onClick={onButtonClick}>Show Modal</button>
       {showModal && <p className='display'>Model Content</p>}
       {showModal && <p className='answer'>This is the model content.</p>}
